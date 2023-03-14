678e0374ba45402f15568a97f327adabf205500c
Axl Cruz <acruz8344@gmail.com>
2023-03-14 16:39:45 -0400

    Added more info to the pages when they're being loaded

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -4,7 +4,10 @@ export const Page = (props) => {
 
   return <>
     <h1>{props.page.title}</h1>
+    <h3>{props.page.author}</h3>
     <h3>{props.page.content}</h3>
+    <h3>{props.page.tags}</h3>
+    <h3>{props.page.createdAt}</h3>
   </>
 } 
 	
